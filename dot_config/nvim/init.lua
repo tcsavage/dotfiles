d0ba592d0c57942bec155ab10ed004f9b607f777
Tom Savage <tcsavage@gmail.com>
2025-02-17 08:55:38 +0000

    [nvim] Switch to ruff for Python linting anf formatting

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -546,7 +546,7 @@ require("lazy").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
 				-- Conform can also run multiple formatters sequentially
-				-- python = { "isort", "black" },
+				python = { "ruff_format", "ruff_organaize_imports" },
 				--
 				-- You can use a sub-list to tell conform to run *until* a formatter
 				-- is found.
